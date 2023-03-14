0b860d4573ecd028f706b8ed2416d26495a667c1
William-Misiaszek <wmisiasz@gmail.com>
2023-03-13 17:21:14 -0700

    Log-in + Register

---
+++ RememberMe/Controllers/LogInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LogInViewController.swift
+//  RememberMe
+//
+//  Created by William Misiaszek on 3/13/23.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
