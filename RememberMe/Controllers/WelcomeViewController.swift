d09c8aa4cc235ddfaf1143f9cf93616dbd2a46a2
William-Misiaszek <wmisiasz@gmail.com>
2023-03-16 13:57:12 -0700

    Start Screen V1

--- RememberMe/Controllers/WelcomeViewController.swift
+++ RememberMe/Controllers/WelcomeViewController.swift
@@ -6,16 +6,55 @@
 //
 
 import UIKit
-
 class WelcomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
+    @IBOutlet weak var StartTitle: UILabel!
+    var currentCharacterIndex = 0
+      var displayText: String = "Namey"
+      var animationTimer: Timer?
 
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          
+          // Initialize the StartTitle with an empty string
+          StartTitle.text = ""
+          
+          // Start the animation
+          startTextAnimation()
+      }
+      
+      func startTextAnimation() {
+          // Invalidate any existing timer
+          animationTimer?.invalidate()
+          
+          // Create a new timer
+          animationTimer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true) { [weak self] timer in
+              guard let self = self else { return }
+              
+              if self.currentCharacterIndex < self.displayText.count {
+                  // Get the next character from the displayText string
+                  let nextCharacter = self.displayText[self.displayText.index(self.displayText.startIndex, offsetBy: self.currentCharacterIndex)]
+                  
+                  // Append the character to the StartTitle text
+                  self.StartTitle.text?.append(nextCharacter)
+                  
+                  // Increment the currentCharacterIndex
+                  self.currentCharacterIndex += 1
+              } else {
+                  // Stop the timer when all characters have been displayed
+                  timer.invalidate()
+              }
+          }
+      }
+      
+      // Call this function to restart the animation
+      func restartTextAnimation() {
+          currentCharacterIndex = 0
+          StartTitle.text = ""
+          startTextAnimation()
+      }
+  }
     /*
     // MARK: - Navigation
 
@@ -26,4 +65,4 @@ class WelcomeViewController: UIViewController {
     }
     */
 
-}
+
