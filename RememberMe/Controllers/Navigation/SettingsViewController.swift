18342b639a0e05eb58958fe832e2e5960b3253bf
William-Misiaszek <wmisiasz@gmail.com>
2024-06-29 23:23:26 -0700

    Settings Refreshed No Friends

--- RememberMe/Controllers/Navigation/SettingsViewController.swift
+++ RememberMe/Controllers/Navigation/SettingsViewController.swift
@@ -21,7 +21,7 @@ class AppState {
 }
 
 
-class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SettingsViewController: UIViewController {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return friendRequests.count + acceptedFriends.count
     }
@@ -86,20 +86,6 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     }
 
 
-    // Function to load accepted friends
-          private func loadAcceptedFriends() {
-              guard let currentUserEmail = Auth.auth().currentUser?.email else { return }
-              let db = Firestore.firestore()
-              let userRef = db.collection("users").document(currentUserEmail)
-              userRef.getDocument { [weak self] (document, error) in
-                  if let document = document, document.exists, let friends = document.data()?["friends"] as? [String] {
-                      self?.acceptedFriends = friends
-                      self?.friendRequestsTableView.reloadData()
-                  } else {
-                      print("Error loading accepted friends: \(error?.localizedDescription ?? "Unknown error")")
-                  }
-              }
-          }
 
 
     
@@ -145,74 +131,54 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         backgroundView.backgroundColor = UIColor(patternImage: UIImage(named: "starry_night")!) // Replace "starry_night" with your image file name
         view.insertSubview(backgroundView, at: 0)
         
-        // Load the friend requests and accepted friends from Firestore
-        loadFriendRequests()
-        loadAcceptedFriends()
     }
     
 
     
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        friendRequestsTableView.layer.cornerRadius = 10
-        friendRequestsTableView.layer.masksToBounds = true
-        
-        if let userEmailString = Auth.auth().currentUser?.email {
-            userEmail.text = userEmailString
-        } else {
-            userEmail.text = "Not logged in"
-        }
-        
-        
-        // Set the delegate and data source for your friendRequestsTableView
-        friendRequestsTableView.delegate = self
-        friendRequestsTableView.dataSource = self
-        
-        // Load the friend requests from Firestore
-        loadFriendRequests()
-        // Initialize audio player with the new path
-        if let path = Bundle.main.path(forResource: "eastersong", ofType: "mp3") {
-            let url = URL(fileURLWithPath: path)
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOf: url)
-                print("Audio player initialized.")
-            } catch {
-                print("Could not initialize audio player: \(error.localizedDescription)")
-            }
-        } else {
-            print("Could not find audio file.")
-        }
-        
-        
-        
-        
-        // Add UITapGestureRecognizer to catImage
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
-        catImage.isUserInteractionEnabled = true
-        catImage.addGestureRecognizer(tapGestureRecognizer)
-        
-        // Add UITapGestureRecognizer to betaTap label
-        let labelTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(labelTapped))
-        betaTap.isUserInteractionEnabled = true
-        betaTap.addGestureRecognizer(labelTapGestureRecognizer)
-        
-        // Add UILongPressGestureRecognizer to betaTap label
-        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(labelLongPressed))
-        longPressGestureRecognizer.minimumPressDuration = 1.0 // 1 second
-        betaTap.addGestureRecognizer(longPressGestureRecognizer)
-        
-        
-    }
-    // Do any additional setup after loading the view.
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        audioPlayer?.stop() // Stop the song when view will disappear
-        print("Stopped the song.")
-    }
-    
+           super.viewDidLoad()
+           
+           if let userEmailString = Auth.auth().currentUser?.email {
+               userEmail.text = userEmailString
+           } else {
+               userEmail.text = "Not logged in"
+           }
+           
+           // Initialize audio player with the new path
+           if let path = Bundle.main.path(forResource: "eastersong", ofType: "mp3") {
+               let url = URL(fileURLWithPath: path)
+               do {
+                   audioPlayer = try AVAudioPlayer(contentsOf: url)
+                   print("Audio player initialized.")
+               } catch {
+                   print("Could not initialize audio player: \(error.localizedDescription)")
+               }
+           } else {
+               print("Could not find audio file.")
+           }
+           
+           // Add UITapGestureRecognizer to catImage
+           let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
+           catImage.isUserInteractionEnabled = true
+           catImage.addGestureRecognizer(tapGestureRecognizer)
+           
+           // Add UITapGestureRecognizer to betaTap label
+           let labelTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(labelTapped))
+           betaTap.isUserInteractionEnabled = true
+           betaTap.addGestureRecognizer(labelTapGestureRecognizer)
+           
+           // Add UILongPressGestureRecognizer to betaTap label
+           let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(labelLongPressed))
+           longPressGestureRecognizer.minimumPressDuration = 1.0 // 1 second
+           betaTap.addGestureRecognizer(longPressGestureRecognizer)
+       }
+       
+       override func viewWillDisappear(_ animated: Bool) {
+           super.viewWillDisappear(animated)
+           audioPlayer?.stop() // Stop the song when view will disappear
+           print("Stopped the song.")
+       }
     
        
        @IBAction func deleteAccountButtonTapped(_ sender: UIButton) {
@@ -263,36 +229,9 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
             }
         }
   
-
-    @IBAction func addFriendButtonTapped(_ sender: UIButton) {
-        loadAllUserEmailsFromUsersCollection()
-
-        // UIAlertController to send a friend request
-          let alertController = UIAlertController(title: "Add Friend", message: "Enter your friend's email:", preferredStyle: .alert)
-          
-          alertController.addTextField { textField in
-              textField.placeholder = "Friend's email"
-          }
-          
-          let confirmAction = UIAlertAction(title: "OK", style: .default) { [weak self, weak alertController] _ in
-              guard let textField = alertController?.textFields?.first,
-                    let friendEmail = textField.text, !friendEmail.isEmpty else {
-                  print("The email field was empty.")
-                  return
-              }
-              self?.sendFriendRequest(to: friendEmail)
-          }
-          
-          let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-          
-          alertController.addAction(confirmAction)
-          alertController.addAction(cancelAction)
-          
-          present(alertController, animated: true)
-      }
     
+//MARK: - END IBACTIONS
     
-    // This function handles the logic for sending a friend request
     // This function handles the logic for sending a friend request
     private func sendFriendRequest(to recipientEmail: String) {
         guard let currentUserEmail = Auth.auth().currentUser?.email?.replacingOccurrences(of: ",", with: ".") else {
@@ -364,197 +303,6 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     }
 
 
-    
-    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        if indexPath.row < friendRequests.count {
-            let acceptAction = UIContextualAction(style: .normal, title: "Accept") { [weak self] (action, view, completionHandler) in
-                self?.acceptFriendRequest(at: indexPath.row)
-                completionHandler(true)
-            }
-            acceptAction.backgroundColor = .green
-            return UISwipeActionsConfiguration(actions: [acceptAction])
-        } else {
-            // No action for accepted friends in leading swipe
-            return nil
-        }
-    }
-
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let deleteAction = UIContextualAction(style: .destructive, title: "Remove") { [weak self] (action, view, completionHandler) in
-            if indexPath.row < self?.friendRequests.count ?? 0 {
-                self?.rejectFriendRequest(at: indexPath.row)
-            } else {
-                self?.removeAcceptedFriend(at: indexPath.row - (self?.friendRequests.count ?? 0))
-            }
-            completionHandler(true)
-        }
-        return UISwipeActionsConfiguration(actions: [deleteAction])
-    }
-
-    // New function to handle the removal of an accepted friend
-    private func removeAcceptedFriend(at index: Int) {
-        guard let currentUserEmail = Auth.auth().currentUser?.email else {
-            print("Current user email not found.")
-            return
-        }
-        
-        let friendEmail = acceptedFriends[index]
-        let db = Firestore.firestore()
-        let currentUserRef = db.collection("users").document(currentUserEmail)
-
-        // Update the current user's friends array to remove the friend
-        currentUserRef.updateData([
-            "friends": FieldValue.arrayRemove([friendEmail])
-        ]) { [weak self] error in
-            if let error = error {
-                print("Error removing friend: \(error)")
-            } else {
-                print("Friend removed successfully.")
-                self?.acceptedFriends.remove(at: index)
-                self?.friendRequestsTableView.reloadData()
-            }
-        }
-    }
-
-    // Logic to accept a friend request.
-    private func acceptFriendRequest(at index: Int) {
-        guard let currentUserEmail = Auth.auth().currentUser?.email else {
-            print("Current user email not found.")
-            return
-        }
-
-        let friendEmail = friendRequests[index]
-        let db = Firestore.firestore()
-        let currentUserRef = db.collection("users").document(currentUserEmail)
-
-        // Start a batch to perform multiple write operations as one transaction
-        let batch = db.batch()
-
-        // Add the current user's updates to the batch
-        batch.updateData([
-            "friendRequests": FieldValue.arrayRemove([friendEmail]),
-            "friends": FieldValue.arrayUnion([friendEmail])
-        ], forDocument: currentUserRef)
-
-        // Find the friend's user document
-        let friendRef = db.collection("users").whereField("email", isEqualTo: friendEmail)
-        friendRef.getDocuments { [weak self] (querySnapshot, error) in
-            if let error = error {
-                print("Error finding user: \(error)")
-            } else if let friendDocument = querySnapshot?.documents.first {
-                // Add the friend's updates to the batch
-                batch.updateData([
-                    "friends": FieldValue.arrayUnion([currentUserEmail])
-                ], forDocument: friendDocument.reference)
-
-                // Commit the batch inside the completion block of getDocuments
-                batch.commit { error in
-                    if let error = error {
-                        print("Error accepting friend request: \(error)")
-                    } else {
-                        print("Friend request accepted.")
-                        // Move the updates for local arrays and table view here
-                        self?.updateLocalFriendListsAndTableView(for: friendEmail, at: index)
-                    }
-                }
-            } else {
-                print("User with email \(friendEmail) does not exist")
-            }
-        }
-    }
-
-    private func updateLocalFriendListsAndTableView(for friendEmail: String, at index: Int) {
-        self.acceptedFriends.append(friendEmail)
-        self.friendRequests.remove(at: index)
-        // Perform table view updates on the main thread
-        DispatchQueue.main.async {
-            self.friendRequestsTableView.performBatchUpdates({
-                let indexPathForDeletion = IndexPath(row: index, section: 0)
-                let indexPathForInsertion = IndexPath(row: self.acceptedFriends.count - 1, section: 0)
-
-                self.friendRequestsTableView.deleteRows(at: [indexPathForDeletion], with: .automatic)
-                self.friendRequestsTableView.insertRows(at: [indexPathForInsertion], with: .automatic)
-            }, completion: nil)
-        }
-    }
-
-      private func animateCellBounce(_ cell: UITableViewCell) {
-          let animationDuration = 0.5
-          let animationDelay = 0.0
-          let animationSpringDamping: CGFloat = 0.6
-          let animationInitialSpringVelocity: CGFloat = 0.1
-
-          // Start with the cell frame 30 points above its final resting place
-          cell.transform = CGAffineTransform(translationX: 0, y: -30)
-          cell.alpha = 0
-
-          // Animate with a bounce effect
-          UIView.animate(
-              withDuration: animationDuration,
-              delay: animationDelay,
-              usingSpringWithDamping: animationSpringDamping,
-              initialSpringVelocity: animationInitialSpringVelocity,
-              options: [],
-              animations: {
-                  // End at a transform identity to be in the final position
-                  cell.transform = CGAffineTransform.identity
-                  cell.alpha = 1
-              },
-              completion: nil
-          )
-      }
-    
-    // Logic to reject a friend request.
-    private func rejectFriendRequest(at index: Int) {
-        guard let currentUserEmail = Auth.auth().currentUser?.email else {
-            print("Current user email not found.")
-            return
-        }
-        
-        let friendEmail = friendRequests[index]
-        let db = Firestore.firestore()
-        let currentUserRef = db.collection("users").document(currentUserEmail)
-        
-        // Remove the friend's email from the current user's friendRequests array
-        currentUserRef.updateData([
-            "friendRequests": FieldValue.arrayRemove([friendEmail])
-        ]) { [weak self] error in
-            if let error = error {
-                print("Error rejecting friend request: \(error)")
-            } else {
-                print("Friend request rejected.")
-                self?.friendRequests.remove(at: index)
-                self?.friendRequestsTableView.reloadData()
-            }
-        }
-    }
-    
-    func addFriend(friendEmail: String) {
-        guard let currentUserEmail = Auth.auth().currentUser?.email else {
-            return // Optionally add error handling
-        }
-        
-        let db = Firestore.firestore()
-        let userRef = db.collection("users").document(currentUserEmail)
-        
-        userRef.updateData([
-            "friendRequests": FieldValue.arrayUnion([friendEmail])
-        ]) { error in
-            if let error = error {
-                // Handle error
-            } else {
-                // Update the UI to reflect the friend request
-                self.updateFriendRequestsListView()
-            }
-        }
-    }
-
-    
-      
-      private func updateFriendRequestsListView() {
-          // The function to refresh the list of friend requests
-          loadFriendRequests()
-      }
     
     @objc private func imageTapped() {
         
